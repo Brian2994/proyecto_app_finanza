cd61ab75fe62cefe94855c10121e50a2ee3d68b1
Brian2994 <brianpari29@gmail.com>
2025-01-03 02:57:18 -0300

    corregimos la fecha y Ingreso/Gasto

--- movimientos.py
+++ movimientos.py
@@ -1,17 +1,26 @@
 # Lógica de los movimientos
 
-import datetime
+from datetime import datetime
 
 # Lista para almacenar los movimientos
 movimientos = []
 
-def agregar_movimiento(descripcion, monto):
-    # Registrar el movimiento
-    fecha = datetime.date.today().strftime('%d/%m/%Y')
-    movimientos.append({'descripcion': descripcion, 'monto': monto, 'fecha': fecha})
+# Función para agregar un movimiento
+def agregar_movimiento(descripcion, monto, tipo):
+    fecha_actual = datetime.now().strftime('%d/%m/%Y')
+    
+    if tipo == 'Ingreso':
+        # Agregar el ingreso (monto positivo)
+        movimientos.append({'descripcion': descripcion, 'monto': monto, 'tipo': tipo, 'fecha': fecha_actual})
+    elif tipo == 'Gasto':
+        # Agregar el gasto (monto negativo)
+        movimientos.append({'descripcion': descripcion, 'monto': monto, 'tipo': tipo, 'fecha': fecha_actual})
 
 def calcular_saldo():
-    return sum(m['monto'] for m in movimientos)
+    saldo = 0
+    for movimiento in movimientos:
+        saldo += movimiento['monto'] # suma los ingresos y resta los gastos
+    return saldo
 
 def obtener_historial():
     return movimientos
